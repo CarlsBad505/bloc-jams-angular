5f825a606b1e30ce6a65a7b797948019f1a462a4
Carl Truong <Carl@tabrific.com>
2016-03-16 21:25:52 -0600

    create Fixture.js factory service

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -2,7 +2,7 @@
 	function CollectionCtrl() {
 		this.albums = [];
 		for (var i=0; i < 12; i++) {
-			this.albums.push(angular.copy(albumPicasso)); // How does it know to grab albumPicasso from fixtures.js??
+			this.albums.push(angular.copy(albumPicasso));
 		}
 	}
 	
---
+++ app/scripts/services/Fixtures.js
@@ -0,0 +1,60 @@
+(function() {
+	function Fixtures() {
+		var Fixtures = {};
+		
+		var albumPicasso = {
+			title: 'The Colors',
+			artist: 'Pablo Picasso',
+			label: 'Cubism',
+			year: '1881',
+			albumArtUrl: '/assets/images/album_covers/01.png',
+			songs: [
+				{ title: 'Blue', duration: 161.71, audioUrl: '/assets/music/blue' },
+				{ title: 'Green', duration: 103.96, audioUrl: '/assets/music/green' },
+				{ title: 'Red', duration: 268.45, audioUrl: '/assets/music/red' },
+				{ title: 'Pink', duration: 153.14, audioUrl: '/assets/music/pink' },
+				{ title: 'Magenta', duration: 374.22, audioUrl: '/assets/music/magenta' }
+			]
+		};
+
+		var albumTidesOfMan = {
+			title: 'Young and Courageous',
+			artist: 'Tides of Man',
+			label: 'Firefly',
+			year: '2014',
+			albumArtUrl: '/assets/images/album_covers/11.png',
+			songs: [
+				{ title: 'Desolate. Magnificent.', duration: '3:18' },
+				{ title: 'Mountain House', duration: '2:52' },
+				{ title: 'Drift', duration: '5:05'},
+				{ title: 'Young and Courageous', duration: '5:39' },
+				{ title: 'All the Years', duration: '2:07'}
+			]
+		};
+
+		var albumMarconi = {
+			title: 'The Telephone',
+			artist: 'Guglielmo Marconi',
+			label: 'EM',
+			year: '1909',
+			albumArtUrl: '/assets/images/album_covers/20.png',
+			songs: [
+				{ title: 'Hello, Operator?', duration: '1:01' },
+				{ title: 'Ring, ring, ring', duration: '5:01' },
+				{ title: 'Fits in your pocket', duration: '3:21'},
+				{ title: 'Can you hear me now?', duration: '3:14' },
+				{ title: 'Wrong phone number', duration: '2:15'}
+			]
+		};
+		
+		Fixtures.getAlbum = function() { //why don't we use this.getAlbum instead of Fixtures.getAlbum?
+			return albumPicasso;	
+		};
+		
+		return Fixtures;
+	}
+	
+	angular
+		.module('blocJams')
+		.factory('Fixtures', Fixtures);
+})();
