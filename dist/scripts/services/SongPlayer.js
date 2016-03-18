e8c9fa4c75f52e5ed38985a23814b282f4827671
Carl Truong <Carl@tabrific.com>
2016-03-17 20:00:07 -0600

    add new private function 'playSong' to SongPlayer.js

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -33,11 +33,20 @@
 			currentSong = song;
 		};
 		
+		/**
+		 * @function playSong
+		 * @desc Plays new audio files as currentBuzzObject and sets song.playing property to true
+		 * @param {Object} song
+		 */
+		var playSong = function(song) {
+			currentBuzzObject.play();
+			song.playing = true;
+		};
+		
 		SongPlayer.play = function(song) {
 			if (currentSong !== song) {
 				setSong(song);
-				currentBuzzObject.play();
-				song.playing = true;
+				playSong(song);
 			} else if (currentSong === song) {
 				if (currentBuzzObject.isPaused()) {
 					currentBuzzObject.play();
